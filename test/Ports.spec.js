513c8ff812f3c4c95b11cb7711b3c868a2b4554f
Rogério Chaves <rogeriocfj@gmail.com>
2016-03-03 00:55:59 -0300

    Fix ports specs

--- test/Ports.spec.js
+++ test/Ports.spec.js
@@ -3,10 +3,14 @@ import { spy } from 'sinon';
 import Ports from '../src/Ports';
 
 describe('Ports', () => {
-  let ports, requestAddTweet, receivedData, requestPublishHead, receivedPublish;
+  let ports, requestAddTweet, receivedData, requestPublishHead, receivedPublishHead, requestPublishTweet, receivedPublishTweet;
 
   beforeEach(() => {
-    Ports.__Rewire__('hash', () => 'myhash');
+    global.localStorage = {
+      publicKey: '1b5a526a05953cff95dbf6c160cc3c621a4c12f360c111c5c44b01c925d24167',
+      secretKey: 'b80cb5444cd32ca171381244bc34fe0950740cbbd4c9c7eee7730822071382588cb57de6a8d71a1d48e7290061eb667be454986ca0b8553599abaa43761fb9a4'
+    };
+
     Ports.__Rewire__('initialData', () => 'initialData');
     Ports.__Rewire__('publish', (item, fn) => fn(null, item));
 
@@ -20,8 +24,14 @@ describe('Ports', () => {
       requestPublishHead: {
         subscribe: (fn) => { requestPublishHead = fn }
       },
-      publishStream: {
-        send: (data) => { receivedPublish = data }
+      publishHeadStream: {
+        send: (data) => { receivedPublishHead = data }
+      },
+      requestPublishTweet: {
+        subscribe: (fn) => { requestPublishTweet = fn }
+      },
+      publishTweetStream: {
+        send: (data) => { receivedPublishTweet = data }
       }
     };
   });
@@ -38,18 +48,26 @@ describe('Ports', () => {
     requestAddTweet({ data: { head: { hash: 'myhash', next: [] }, tweets: [] }, text: 'hello world' });
 
     expect(receivedData).to.deep.equal({
-      head: { hash: 'myhash', next: ['65131bd315f30324b0c0d6cbf2ccc058ea7523ec'] },
+      head: { hash: 'myhash', next: ['dcbfa7a4332149de74822303ad05b1c77d0f72f0'] },
       tweets: [
-        { hash: '65131bd315f30324b0c0d6cbf2ccc058ea7523ec', t: 'hello world', next: [] }
+        { hash: 'dcbfa7a4332149de74822303ad05b1c77d0f72f0', t: 'hello world', next: [] }
       ]
     });
   });
 
-  it('publishes data, sending the hashes of data being published back', () => {
+  it('publishes head, sending the hashes of data being published back', () => {
     Ports.setup(ports);
 
     requestPublishHead({ data: 'foo' });
 
-    expect(receivedPublish).to.deep.equal({ data: 'foo' });
+    expect(receivedPublishHead).to.deep.equal({ data: 'foo' });
+  });
+
+  it('publishes tweet, sending the hashes of data being published back', () => {
+    Ports.setup(ports);
+
+    requestPublishTweet({ data: 'foo' });
+
+    expect(receivedPublishTweet).to.deep.equal({ data: 'foo' });
   });
 });

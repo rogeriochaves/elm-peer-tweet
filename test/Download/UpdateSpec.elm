eaf1b70a59c126ce6eeb771192f32a54084da71a
Rogério Chaves <rogeriocfj@gmail.com>
2016-03-07 09:28:33 -0300

    Extract helpers to TestHelper file

--- test/Download/UpdateSpec.elm
+++ test/Download/UpdateSpec.elm
@@ -5,31 +5,9 @@ import Data.Model exposing (initialModel)
 import Action exposing (..)
 import Download.Action exposing (..)
 import ElmTestBDDStyle exposing (..)
-import ElmTest exposing (Assertion)
 import Effects exposing (toTask)
 import Task exposing (Task, andThen, sequence)
-import Signal.Extra exposing (combine)
-
-signalExpect : (Signal c, Task a b) -> (c -> d -> Bool) -> d -> Signal (Task a Assertion)
-signalExpect (signal, task) toBe expected =
-  signal
-    |> Signal.map (\value ->
-         task `andThen` (always <| Task.succeed <| expect value toBe expected)
-       )
-
-signalIt : String -> Signal (Task a Assertion) -> Signal (Task a Test)
-signalIt description taskSignal =
-  taskSignal
-    |> Signal.map (Task.map <| it description)
-
-signalDescribe : String -> List (Signal (Task a Test)) -> Signal (Task a Test)
-signalDescribe description testsSignals =
-  testsSignals
-    |> combine
-    |> Signal.map (Task.sequence >> (Task.map <| describe description))
-
-dumpMailbox : Signal.Mailbox (List Action.Action)
-dumpMailbox = Signal.mailbox [NoOp]
+import TestHelpers exposing (signalExpect, signalIt, signalDescribe)
 
 tests : Signal (Task Effects.Never Test)
 tests =
@@ -44,3 +22,6 @@ tests =
         in
           signalExpect (mailbox.signal, task) toBe (ActionForDownload <| DownloadTweet "foobar")
     ]
+
+dumpMailbox : Signal.Mailbox (List Action.Action)
+dumpMailbox = Signal.mailbox [NoOp]

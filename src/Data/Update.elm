3a6ff48c4efbd3e054883bf2602839de434edd9f
Rogério Chaves <rogeriocfj@gmail.com>
2016-03-11 20:04:29 -0300

    Add Data effects

--- src/Data/Update.elm
+++ src/Data/Update.elm
@@ -4,6 +4,10 @@ import Action as RootAction exposing (..)
 import Data.Action as DataAction exposing (..)
 import Data.Model exposing (Model)
 import Account.Update as AccountUpdate
+import Account.Model as AccountModel exposing (HeadHash)
+import Account.Action as AccountAction
+import Task
+import Effects exposing (Effects)
 
 
 update : RootAction.Action -> Model -> Model
@@ -11,16 +15,33 @@ update action model =
   case action of
     ActionForData (DataAction.ActionForAccount hash accountAction) ->
       { model
-        | accounts =
-            List.map
-              (\x ->
-                if x.head.hash == hash then
-                  AccountUpdate.update (RootAction.ActionForAccount accountAction) x
-                else
-                  x
-              )
-              model.accounts
+        | accounts = updateAccount model hash accountAction
       }
 
     _ ->
       model
+
+
+updateAccount : Model -> HeadHash -> AccountAction.Action -> List AccountModel.Model
+updateAccount model hash action =
+  List.map
+    (\x ->
+      if x.head.hash == hash then
+        AccountUpdate.update (RootAction.ActionForAccount action) x
+      else
+        x
+    )
+    model.accounts
+
+
+effects : Signal.Address RootAction.Action -> RootAction.Action -> Model -> Effects RootAction.Action
+effects jsAddress action _ =
+  case action of
+    ActionForData (DataAction.ActionForAccount hash accountAction) ->
+      Signal.send jsAddress (ActionForData (DataAction.ActionForAccount hash accountAction))
+        |> Task.toMaybe
+        |> Task.map (always NoOp)
+        |> Effects.task
+
+    _ ->
+      Effects.none

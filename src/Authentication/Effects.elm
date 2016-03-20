c52fd2d7e244b81f38624071588ce0b2967f2d4e
Rogério Chaves <rogeriocfj@gmail.com>
2016-03-19 22:15:10 -0300

    Start download logged in hash

--- src/Authentication/Effects.elm
+++ src/Authentication/Effects.elm
@@ -1,14 +1,19 @@
 module Authentication.Effects (effects) where
 
 import Action as RootAction exposing (..)
-import Authentication.Action exposing (Action(CreateKeys))
+import Authentication.Action exposing (Action(CreateKeys, DoneLogin))
 import Task
 import Effects exposing (Effects)
+import Download.Action exposing (Action(DownloadHead))
 
 
 effects : Signal.Address RootAction.Action -> RootAction.Action -> Effects RootAction.Action
 effects jsAddress action =
   case action of
+    ActionForAuthentication (DoneLogin hash) ->
+      Task.succeed (ActionForDownload <| DownloadHead hash)
+        |> Effects.task
+
     ActionForAuthentication authenticationAction ->
       Signal.send jsAddress (ActionForAuthentication authenticationAction)
         |> Task.map (always RootAction.NoOp)

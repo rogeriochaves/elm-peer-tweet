8d1c397b65ef0f239067126047bb449cd6188410
Rogério Chaves <rogeriocfj@gmail.com>
2016-03-25 12:35:44 -0300

    Show current selected page on the side bar

--- src/Sidebar/View/Sidebar.elm
+++ src/Sidebar/View/Sidebar.elm
@@ -9,30 +9,57 @@ import Publish.View.PublishProgress as PublishProgress
 import Download.View.DownloadProgress as DownloadProgress
 import Router.Routes exposing (Sitemap(..))
 import Router.Action exposing (Action(UpdatePath))
+import Router.Model exposing (Page(..))
 import Timeline.View.Avatar as Avatar
 import Account.Model as Account
 
 
 view : Signal.Address RootAction.Action -> Model -> Account.Model -> Html
 view address model userAccount =
-  div
-    [ class "sidebar" ]
-    [ div
-        [ class "sidebar-items blue darken-4" ]
-        [ button
-            [ class "sidebar-button sidebar-avatar", onClick address (ActionForRouter <| UpdatePath <| ProfileRoute userAccount.head.hash) ]
-            [ Avatar.view userAccount.head ]
-        , button
-            [ class "sidebar-button material-icons", onClick address (ActionForRouter <| UpdatePath <| TimelineRoute ()) ]
-            [ text "home" ]
-        , button
-            [ class "sidebar-button material-icons", onClick address (ActionForRouter <| UpdatePath <| FollowingListRoute ()) ]
-            [ text "group" ]
-        , button
-            [ class "sidebar-button material-icons", onClick address (ActionForRouter <| UpdatePath <| SearchRoute ()) ]
-            [ text "search" ]
-        , div [ class "sidebar-space" ] []
-        , PublishProgress.view address model.publish
-        , DownloadProgress.view address model.download
-        ]
-    ]
+  let
+    classes =
+      { home = "", followList = "", search = "" }
+
+    activeClass =
+      case model.router.page of
+        Timeline ->
+          { classes | home = "active" }
+
+        FollowingList ->
+          { classes | followList = "active" }
+
+        Search ->
+          { classes | search = "active" }
+
+        _ ->
+          classes
+  in
+    div
+      [ class "sidebar" ]
+      [ div
+          [ class "sidebar-items blue darken-4" ]
+          [ button
+              [ class "sidebar-button sidebar-avatar"
+              , onClick address (ActionForRouter <| UpdatePath <| ProfileRoute userAccount.head.hash)
+              ]
+              [ Avatar.view userAccount.head ]
+          , button
+              [ class <| "sidebar-button material-icons " ++ activeClass.home
+              , onClick address (ActionForRouter <| UpdatePath <| TimelineRoute ())
+              ]
+              [ text "home" ]
+          , button
+              [ class <| "sidebar-button material-icons " ++ activeClass.followList
+              , onClick address (ActionForRouter <| UpdatePath <| FollowingListRoute ())
+              ]
+              [ text "group" ]
+          , button
+              [ class <| "sidebar-button material-icons " ++ activeClass.search
+              , onClick address (ActionForRouter <| UpdatePath <| SearchRoute ())
+              ]
+              [ text "search" ]
+          , div [ class "sidebar-space" ] []
+          , PublishProgress.view address model.publish
+          , DownloadProgress.view address model.download
+          ]
+      ]

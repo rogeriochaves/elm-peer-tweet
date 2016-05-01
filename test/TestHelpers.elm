e33b3510b16a0c87e8bc20ccfde03e3f69635587
Rogério Chaves <rogeriocfj@gmail.com>
2016-05-01 10:07:45 -0300

    Upgrade elm-test-bdd-style

--- test/TestHelpers.elm
+++ test/TestHelpers.elm
@@ -1,13 +1,12 @@
 module TestHelpers (..) where
 
 import ElmTestBDDStyle exposing (..)
-import ElmTest exposing (Assertion)
 import Task exposing (Task, andThen, sequence)
 import Signal exposing (map, constant)
 import Signal.Extra exposing (combine)
 
 
-expectSignal : ( Signal c, Task a b ) -> (c -> d -> Bool) -> d -> Signal (Task a Assertion)
+expectSignal : ( Signal c, Task a b ) -> (c -> d -> Assertion) -> d -> Signal (Task a Assertion)
 expectSignal ( signal, task ) toBe expected =
   signal
     |> map
@@ -16,7 +15,7 @@ expectSignal ( signal, task ) toBe expected =
         )
 
 
-expectTask : Task a b -> (b -> c -> Bool) -> c -> Signal (Task a Assertion)
+expectTask : Task a b -> (b -> c -> Assertion) -> c -> Signal (Task a Assertion)
 expectTask task toBe expected =
   task
     `andThen` (\value -> Task.succeed <| expect value toBe expected)

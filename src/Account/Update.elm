1c95bbe2af87a5fa6fa277404b134c697f25d075
Rogério Chaves <rogeriocfj@gmail.com>
2016-05-28 09:51:29 -0300

    Migrate utils and account

--- src/Account/Update.elm
+++ src/Account/Update.elm
@@ -6,21 +6,21 @@ import Account.Model exposing (Model, addTweet, addFollowBlock)
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Update account ->
-      if account.head.d > model.head.d then
-        account
-      else
-        model
+    case msg of
+        Update account ->
+            if account.head.d > model.head.d then
+                account
+            else
+                model
 
-    UpdateHead head ->
-      if head.d > model.head.d then
-        { model | head = head }
-      else
-        model
+        UpdateHead head ->
+            if head.d > model.head.d then
+                { model | head = head }
+            else
+                model
 
-    AddTweet tweet ->
-      addTweet model tweet
+        AddTweet tweet ->
+            addTweet model tweet
 
-    AddFollowBlock followBlock ->
-      addFollowBlock model followBlock
+        AddFollowBlock followBlock ->
+            addFollowBlock model followBlock

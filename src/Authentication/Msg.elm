975fbbef6b815429ec33b9fc57d68303e846270d
Rogério Chaves <rogeriocfj@gmail.com>
2016-05-28 10:01:12 -0300

    Migrate authentication model, msg and update

--- src/Authentication/Msg.elm
+++ src/Authentication/Msg.elm
@@ -5,10 +5,10 @@ import Account.Model exposing (HeadHash)
 
 
 type Msg
-  = CreateKeys
-  | DoneCreateKeys { hash : HeadHash, keys : Keys }
-  | UpdatePublicKey String
-  | UpdateSecretKey String
-  | UpdateName String
-  | Login Keys
-  | DoneLogin HeadHash
+    = CreateKeys
+    | DoneCreateKeys { hash : HeadHash, keys : Keys }
+    | UpdatePublicKey String
+    | UpdateSecretKey String
+    | UpdateName String
+    | Login Keys
+    | DoneLogin HeadHash
--- src/Authentication/Update.elm
+++ src/Authentication/Update.elm
@@ -8,44 +8,44 @@ import Accounts.Msg exposing (Msg(CreateAccount, UpdateUserAccount))
 
 update : RootMsg.Msg -> Model -> Model
 update msg model =
-  case msg of
-    MsgForAuthentication authenticationMsg ->
-      updateAuthentication authenticationMsg model
+    case msg of
+        MsgForAuthentication authenticationMsg ->
+            updateAuthentication authenticationMsg model
 
-    MsgForAccounts (UpdateUserAccount account) ->
-      { model | hash = (Just account.head.hash) }
+        MsgForAccounts (UpdateUserAccount account) ->
+            { model | hash = (Just account.head.hash) }
 
-    MsgForAccounts (CreateAccount hash _ _) ->
-      { model | hash = (Just hash) }
+        MsgForAccounts (CreateAccount hash _ _) ->
+            { model | hash = (Just hash) }
 
-    _ ->
-      model
+        _ ->
+            model
 
 
 updateAuthentication : Authentication.Msg -> Model -> Model
 updateAuthentication msg model =
-  let
-    keys =
-      model.keys
-  in
-    case msg of
-      CreateKeys ->
-        model
+    let
+        keys =
+            model.keys
+    in
+        case msg of
+            CreateKeys ->
+                model
 
-      DoneCreateKeys { hash, keys } ->
-        { model | hash = (Just hash), keys = keys }
+            DoneCreateKeys { hash, keys } ->
+                { model | hash = (Just hash), keys = keys }
 
-      UpdatePublicKey publicKey ->
-        { model | keys = { keys | publicKey = publicKey } }
+            UpdatePublicKey publicKey ->
+                { model | keys = { keys | publicKey = publicKey } }
 
-      UpdateSecretKey secretKey ->
-        { model | keys = { keys | secretKey = secretKey } }
+            UpdateSecretKey secretKey ->
+                { model | keys = { keys | secretKey = secretKey } }
 
-      UpdateName name ->
-        { model | name = name }
+            UpdateName name ->
+                { model | name = name }
 
-      Login _ ->
-        model
+            Login _ ->
+                model
 
-      DoneLogin hash ->
-        { model | hash = (Just hash) }
+            DoneLogin hash ->
+                { model | hash = (Just hash) }

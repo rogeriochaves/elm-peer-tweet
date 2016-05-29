96bda9980a9e01d4702458f4521cb1dff5af4bb5
Rogério Chaves <rogeriocfj@gmail.com>
2016-05-29 02:22:08 -0300

    Update settings cmd

---
+++ src/Settings/Cmd.elm
@@ -0,0 +1,28 @@
+module Settings.Cmd exposing (cmds)
+
+import Msg as RootMsg exposing (Msg(MsgForSettings, MsgForAccounts, MsgForPublish))
+import Model exposing (Model)
+import Settings.Msg exposing (Msg(SaveSettings))
+import Accounts.Msg exposing (Msg(UpdateUserAccount))
+import Accounts.Model exposing (getUserAccount)
+import Task exposing (andThen, succeed, perform)
+
+
+cmds : RootMsg.Msg -> Model -> Cmd RootMsg.Msg
+cmds msg model =
+    let
+        updatedUser userAccount head =
+            { userAccount | head = { head | a = model.settings.avatar, d = round model.dateTime.timestamp } }
+
+        updateAccountCmd userAccount =
+            succeed (MsgForAccounts <| UpdateUserAccount <| updatedUser userAccount userAccount.head)
+                |> perform identity identity
+    in
+        case msg of
+            MsgForSettings SaveSettings ->
+                getUserAccount model
+                    |> Maybe.map updateAccountCmd
+                    |> Maybe.withDefault Cmd.none
+
+            _ ->
+                Cmd.none

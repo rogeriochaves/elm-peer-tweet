3cb8ae6a78cfbae17976bec3588303c326ab8c9e
Rogério Chaves <rogeriocfj@gmail.com>
2016-06-03 06:33:05 -0300

    Update test running

--- test/TestRunner.elm
+++ test/TestRunner.elm
@@ -1,72 +1,22 @@
 module Main exposing (..)
 
-import Task exposing (Task, andThen, sequence)
-import Console exposing (run, putStrLn, (>>>))
-import Graphics.Element exposing (Element, show)
 import ElmTest exposing (..)
-import Effects exposing (Never)
-import Signal.Extra exposing (combine)
 import ElmTestBDDStyle exposing (describe, it, expect, toBe)
-import SignalConcatMap exposing ((>>=))
 import Account.UpdateSpec
 import Account.ModelSpec
 import DateTime.View.TimeDifferenceSpec
-import Download.EffectsSpec
-import Publish.EffectsSpec
 import Accounts.ModelSpec
 
 
 simpleTests : List Test
 simpleTests =
-  [ Account.UpdateSpec.tests
-  , Account.ModelSpec.tests
-  , DateTime.View.TimeDifferenceSpec.tests
-  , Accounts.ModelSpec.tests
-  ]
+    [ Account.UpdateSpec.tests
+    , Account.ModelSpec.tests
+    , DateTime.View.TimeDifferenceSpec.tests
+    , Accounts.ModelSpec.tests
+    ]
 
 
-effectsTests : List (Signal (Task Never Test))
-effectsTests =
-  [ Download.EffectsSpec.tests
-  , Publish.EffectsSpec.tests
-  ]
-
-
-testsMailbox : Signal.Mailbox (Maybe Test)
-testsMailbox =
-  Signal.mailbox Nothing
-
-
-port tasks : Signal (Task.Task Never ())
-port tasks =
-  combine effectsTests
-    |> Signal.map solveEffects
-
-
-solveEffects : List (Task Never Test) -> Task Never ()
-solveEffects effects =
-  sequence effects `andThen` signalTests
-
-
-signalTests : List Test -> Task Never ()
-signalTests =
-  List.append simpleTests >> describe "Elm Tests" >> Just >> Signal.send testsMailbox.address
-
-
-main : Signal Element
+main : Program Never
 main =
-  Signal.map (Maybe.map elementRunner >> Maybe.withDefault (show "Running tests")) testsMailbox.signal
-
-
-port runner : Signal (Task.Task x ())
-port runner =
-  let
-    command tests =
-      case tests of
-        Just tests ->
-          Console.run <| (putStrLn "" >>> putStrLn "" >>> consoleRunner tests)
-
-        Nothing ->
-          Console.run <| (putStrLn "Wait...")
-  in
-    testsMailbox.signal >>= command
+    runSuite (describe "Elm Tests" <| simpleTests)
